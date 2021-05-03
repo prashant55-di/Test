232d70c800c2b77ad80c2a4d6b17948036ab1a22
Prashant Choubey <prashantchoubey7912@gmail.com>
2021-05-03 16:16:42 +0530

    First commit

---
+++ eclipse-workspace/Fibonaci series/src/Fibo.java
@@ -0,0 +1,27 @@
+
+public class Fibo {
+
+	    public static void main(String[] args) {
+
+	        int num = 121, a = 0, remainder, originalInteger;
+
+	        originalInteger = num;
+
+	        // reversed integer is stored in variable 
+	        while( num != 0 )
+	        {
+	        /*    remainder = num % 10;
+	            mlmproject990@gmail.com
+	            MlM##990
+	            a = a * 10 + remainder;
+	            num = num/10;*/
+	        }
+
+	        // palindrome if orignalInteger and a are equal
+	        if (originalInteger == a)
+	            System.out.println(originalInteger + " is a palindrome.");
+	        else
+	            System.out.println(originalInteger + " is not a palindrome.");
+
+	    }}
+	
