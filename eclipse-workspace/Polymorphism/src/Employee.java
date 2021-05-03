97704eb9306761897267832451a3052b8a4fead2
Prashant Choubey <prashantchoubey7912@gmail.com>
2021-05-03 18:00:00 +0530

    Second Commit

---
+++ eclipse-workspace/Polymorphism/src/Employee.java
@@ -0,0 +1,22 @@
+//save as Student.java  
+import java.util.*;
+public class Employee{  
+private String name;  
+   
+public String getName(){  
+return name;  
+}  
+public void setName(java.lang.String string) {
+	// TODO Auto-generated method stub
+	this.name=name;	
+}  
+}    
+class best{  
+public static void main(String[] args) throws Exception{  
+Employee e=new Employee();
+
+e.setName("vijay");
+
+System.out.println(e.getName());  
+}  
+}  
