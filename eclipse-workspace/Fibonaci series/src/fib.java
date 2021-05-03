d6e9c36737ebf4213bcd636874f3413add1b48a0
Prashant Choubey <prashantchoubey7912@gmail.com>
2021-05-03 16:44:32 +0530

    First Commit

---
+++ eclipse-workspace/Fibonaci series/src/fib.java
@@ -0,0 +1,22 @@
+
+public class fib {
+
+	public static void main(String[] args) {
+
+        int n = 10, a = 0, b = 1;
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(a + " + " + " "
+            		);
+
+            int sum = a + b;
+            a = b;
+            b = sum;
+        }
+    }
+	
+}
+
+
+    
